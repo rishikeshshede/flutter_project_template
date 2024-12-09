3b501efb80399640e07990f06118e007c2cf3c83
Rishikesh Shede <rishi@Rishi-2.local>
2024-12-09 17:56:05 +0530

    added dynamic environment handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project_template/utils/log_handler_util.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  LogHandlerUtil.isProduction = false;
+void main() async {
+  await dotenv.load(fileName: ".env");
+  LogHandlerUtil.isProduction =
+      dotenv.env['IS_PRODUCTION']?.toLowerCase() == 'true';
   runApp(const MainApp());
 }
 
